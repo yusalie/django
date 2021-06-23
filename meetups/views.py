e82872ee2e0c7b3af2f1994be02a5c0e1e9d60df
yusuf <yusuf.salie96@gmail.com>
2021-06-23 11:53:33 +0200

    update

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def index():
-    return HttpResponse('Hello World!')
+def index(request):
+    return render(request, 'meetups/index.html')
