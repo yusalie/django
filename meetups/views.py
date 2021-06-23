87bbdfb56b95f818ccc15158fe9ff5ca02609df2
yusuf <yusuf.salie96@gmail.com>
2021-06-23 13:03:44 +0200

    update

--- meetups/views.py
+++ meetups/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     meetups = [
-        {'title': 'A first meetup'},
-        {'title': 'A second meetup'},
+        {
+            'title': 'A first meetup', 
+            'location':'New York', 
+            'slug':'a-first-meetup'
+        },
+        {
+            'title': 'A second meetup', 
+            'location':'Japan', 
+            'slug':'a-second-meetup'
+        },
         
     ]
     return render(request, 'meetups/index.html',{
