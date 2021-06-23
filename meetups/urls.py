b25fee8c7aca741f9513e58bada2cf96c87118c5
yusuf <yusuf.salie96@gmail.com>
2021-06-23 15:50:52 +0200

    updated

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index) #our-domain/meetups/
+    path('meetups/', views.index, name='all-meetups'), #our-domain/meetups/
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'), #our-domain/meetups/<dynamic-path-segment>
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -19,3 +19,14 @@ def index(request):
         'show_meetups': True,
         'meetups': meetups
     })
+
+def meetup_details(request, meetup_slug):
+    print(meetup_slug)
+    selected_meetups ={
+            'title':'A first meetup', 
+            'description':'this is the first meetup'
+        }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title':selected_meetups['title'],
+        'meetup_description': selected_meetups['description']
+    })
